8a6e8e503d39af79f7fa70273f720a368a484ba8
root <jdarahthomas@gmail.com>
2023-04-04 20:28:19 +0000

    took out cross_origin for app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@ Flask App that integrates with AirBnB static HTML Template
 """
 from api.v1.views import app_views
 from flask import Flask, jsonify, make_response, render_template, url_for
-from flask_cors import CORS, cross_origin
+from flask_cors import CORS
 from flasgger import Swagger
 from models import storage
 import os
