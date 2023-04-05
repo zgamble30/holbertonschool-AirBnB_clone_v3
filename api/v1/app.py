0c7f6f6e3c91af061c1fd8dc4cea1ac45249f50b
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-05 11:00:44 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,29 +1,18 @@
 #!/usr/bin/python3
-"""
-    Entry point for the API
-"""
+"""Flask App"""
 import os
 from flask import Flask, jsonify
-from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    """Calls storage close"""
-    storage.close()
-
+app.register_blueprint(app_views)
 
 @app.errorhandler(404)
-def page_not_found(e):
-    """Returns JSON 404 status"""
-    return jsonify(error="Not found"), 404
-
+def not_found(error):
+    """Handle 404 errors with JSON response"""
+    return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=getenv('HBNB_API_PORT', 5000),
-            threaded=True)
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    app.run(host=host, port=port)
