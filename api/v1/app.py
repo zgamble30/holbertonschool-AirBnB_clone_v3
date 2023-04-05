4a9dad985cd2c02cdcc63827ded3cf00eeef1a34
root <jdarahthomas@gmail.com>
2023-04-05 21:37:50 +0000

    task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,8 @@
 import os
 from flask import Flask, jsonify
 from api.v1.views import app_views
-
+from models import storage
+from flask_cors import CORS
 
 # create an instance of Flask
 app = Flask(__name__)
@@ -14,6 +15,13 @@ app.register_blueprint(app_views)
 def not_found(error):
     """Handle 404 errors with JSON response"""
     return jsonify({"error": "Not found"}), 404
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """method closes storage session"""
+    storage.close()
+
 
 if __name__ == "__main__":
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
