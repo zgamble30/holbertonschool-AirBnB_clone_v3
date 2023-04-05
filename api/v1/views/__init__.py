a83bd695fc093b0484424d7bd33e98f79abd092c
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-05 10:54:35 -0500

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,12 +2,6 @@
 """Define a blueprint for the API v1"""
 """import flask views"""
 from api.v1.views.index import app_views
-from api.v1.views.states import *
-from api.v1.views.cities import *
-from api.v1.views.amenities import *
-from api.v1.views.users import *
-from api.v1.views.places import *
-from api.v1.views.places_reviews import *
 from flask import Blueprint, abort, request
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
