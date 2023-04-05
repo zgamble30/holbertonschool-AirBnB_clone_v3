3462e2a1de0e54df8687eca85482338270a1d037
root <jdarahthomas@gmail.com>
2023-04-05 15:45:27 +0000

    views/init file fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-
+"""Define a blueprint for the API v1"""
+"""import flask views"""
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -8,8 +9,7 @@ from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
 from flask import Blueprint, abort, request
-"""Define a blueprint for the API v1"""
-"""import flask views"""
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 
