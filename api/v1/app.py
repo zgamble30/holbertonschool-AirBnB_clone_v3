91edcdf91910d1bc75a3548ab578d422adae5eb1
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-05 10:50:25 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response
 from flask_cors import CORS
 from models import storage
-from api.v1.views import app_views
+from api.v1.views.index import app_views
 from os import getenv as env
 
 
