5af6e382f3d929a0d570ca42223db51e90d2869b
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-05 10:52:13 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,38 +1,28 @@
 #!/usr/bin/python3
-""" Creating an instance of Flask """
-from flask import Flask, make_response
-from flask_cors import CORS
+"""
+    Entry point for the API
+"""
+from flask import Flask, jsonify
 from models import storage
-from api.v1.views.index import app_views
-from os import getenv as env
+from api.v1.views import app_views
 
-
-# create an instance of Flask
 app = Flask(__name__)
-CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
-# register blueprint
-app.register_blueprint(app_views)
-
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """handles 404 scenario (page not found)"""
-    code = exception.__str__().split()[0]
-    message = {"error": "Not found"}
-    return make_response(message, code)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ method closes storage  session"""
+def teardown(exception):
+    """Calls storage close"""
     storage.close()
 
 
-def start_flask():
-    """ start flask """
-    app.run(host=env('HBNB_API_HOST'),
-            port=env('HBNB_API_PORT'),
-            threaded=True)
+@app.errorhandler(404)
+def page_not_found(e):
+    """Returns JSON 404 status"""
+    return jsonify(error="Not found"), 404
+
 
 if __name__ == "__main__":
-    start_flask()
+    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
+            port=getenv('HBNB_API_PORT', 5000),
+            threaded=True)
