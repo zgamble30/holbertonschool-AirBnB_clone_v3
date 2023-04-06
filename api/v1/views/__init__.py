5a873628afacb3df7c59f31170dfae5104b8b15a
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-06 15:35:57 -0500

    Update __init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""Define a blueprint for the API v1"""
-"""import flask views"""
-from api.v1.views.index import app_views
-from flask import Blueprint, abort, request
+"""creating app_views which is an instance of Blueprint"""
+from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-
-
-def validate_model(model, model_id):
-    """Validate if a model exists"""
-    from models import storage
-    obj = storage.get(model, model_id)
-    if obj is None:
-        abort(404)
-    return obj
-
-
-def get_json(required_fields=[]):
-    """Get the json from the request"""
-    result = request.get_json()
-    if result is None:
-        abort(400, 'Not a JSON')
-    for field in required_fields:
-        if result.get(field) is None:
-            abort(400, 'Missing {}'.format(field))
-    return result
+from api.v1.views.index import *
+from api.v1.views.states import *
+from api.v1.views.places import *
+from api.v1.views.cities import *
+from api.v1.views.amenities import *
+from api.v1.views.users import *
+from api.v1.views.places_reviews import *
