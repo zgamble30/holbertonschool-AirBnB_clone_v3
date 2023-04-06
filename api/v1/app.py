70311ea9b046387b24f3f2298a18437de67d0e7c
zgamble30 <110429755+zgamble30@users.noreply.github.com>
2023-04-06 15:35:10 -0500

    Update app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Flask App"""
-import os
-from flask import Flask, jsonify
-from api.v1.views import app_views
-from models import storage
+""" Creating an instance of Flask """
+from flask import Flask, jsonify, make_response
 from flask_cors import CORS
+from models import storage
+from api.v1.views import app_views
+from os import getenv as env
 
-# create an instance of Flask
+
 app = Flask(__name__)
+CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 app.register_blueprint(app_views)
 
 
 @app.errorhandler(404)
-def not_found(error):
-    """Handle 404 errors with JSON response"""
-    return jsonify({"error": "Not found"}), 404
+def handle_404(exception):
+    """handles 404 scenario (page not found)"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
@@ -24,6 +25,6 @@ def teardown(exception):
 
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
-    app.run(host=host, port=port)
+    app.run(host=env('HBNB_API_HOST'),
+            port=env('HBNB_API_PORT'),
+            threaded=True)
